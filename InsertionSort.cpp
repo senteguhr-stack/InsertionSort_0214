d37442b3b658158b6bff9de48c6f0c658bfdd19d
senteguhr <senteguhr@gmail.com>
2026-03-10 12:15:23 +0700

    menambahkan fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int arr[20];
 int n;
 
+void input (){
+    while (true){
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
