15ec13e9fddd59833f3df399c7c83e6da4672a4a
senteguhr <senteguhr@gmail.com>
2026-04-21 12:07:34 +0700

    tambah struktur

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -1,3 +1,22 @@
 #include <iostream>2
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+ 
