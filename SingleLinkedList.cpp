7855c7eaa91f927a08d2945ebce0bb4b96f74faf
senteguhr <senteguhr@gmail.com>
2026-04-21 12:09:02 +0700

    tambah struktur

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -19,4 +19,29 @@ public:
         START = NULL;
     }
 
- 
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan NIM Mahasiswa: ";
+        cin >> nim;
+
+        Node *Nodebaru = new Node();
+        Nodebaru->noMhs = nim;
+
+        if (START == NULL || START->noMhs >= Nodebaru->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak di ijinkan\n";
+                return;
+            }
+
+            Nodebaru->next = START;
+            START = Nodebaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        
